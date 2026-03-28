3fdc8320a9f42634502b41464e6214d7aa40b582
LE BAR <letruong8017@gmail.com>
2026-03-28 10:10:12 +0700

    feat: create endpoint create user (mock data)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/dto/create-users.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') userId: number) {
+    return { userId };
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return {
+      message: 'create success',
+      email: createUserDto.email,
+      password: createUserDto.password,
+      role: createUserDto.role,
+    };
+  }
+}
